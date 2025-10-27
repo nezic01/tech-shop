ae595d16849084a189259306cf097c844e30bbb5
nezic01 <neza15102001@gmail.com>
2025-10-27 15:49:18 +0100

    Contact form updated

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let fullname = document.getElementById('fname');
 let email = document.getElementById('email');
-
+let product_description = document.getElementById('product_description');
 function checkName(){
    
 
@@ -17,23 +17,29 @@ if (!(isNaN(fullname.value[i]))){
 }
 
 function checkLength(arr){
-    return arr.length === 0;
+    return arr.length == 0;
 
 }
 function checkEmail(){
-    for(let i = 0;i<email.value.length;i++){
-        if(!(email.value[i] =='@')){
-            alert('Email address must have @');
-            break;
-        }
-        else if(email.value[0] == '@'){
-            alert('Email can not start with @');
-            break;
-        }
-        else if(checkLength(email.value)){
-            alert('Please enter your email!');
-            break;
-        }
+            if(!(email.value.includes('@'))){
+                alert('Email must have @')
+            }
+            else if(email.value[0]=='@'){
+                alert('Email can not start with @')
+            }
+            else if ((checkLength(email.value))){
+                alert('Enter your email!')
+               } 
+        
+      
        
     }
-}
+    function checkProductDescription(){
+        if(product_description.value ==null)
+            {
+                alert('Please describe product you want to buy');
+            }
+        
+    }
+
+
